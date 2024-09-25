49eaea78a1fd8bb5f606afb93e05c9ade866ce1e
om24-fi <osvaldomarichal@gmail.com>
2024-09-25 16:45:41 -0300

    guia2dotp.py

--- CodigosDeClases/ejemploOMAYE.py
+++ CodigosDeClases/ejemploOMAYE.py
@@ -1,2 +1,8 @@
-x="Soy una variable del tipo Str asignada por Osvaldo"
-x="Soy una variable tipo del tipo Str asignado por Carlos"
+(separar implementación de aplicación)
+que no sea para pacientes solamentes
+
+Caracteristicas:
+se ordenan los pacientes por prioridad segun el nvl de riesgo  1critico 2moderado 3 bajo
+
+chatgpt: Indica seguir una cola de prioridad
+profe: monticulo de minimos
